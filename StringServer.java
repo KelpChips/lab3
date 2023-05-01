36678ef870343adaad724fd721ca7236f0ce1ecf
KelpChips <123066638+KelpChips@users.noreply.github.com>
2023-05-01 10:31:09 -0700

    sd

---
+++ StringServer.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    String sentences;
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Amazing slayful dancer Renato Number: %d", sentences);
+        } else {
+            System.out.println("Path: " + url.getPath());
+            //adds the word or sentence to list of sentence
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+                sentences += parameters[1];
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
